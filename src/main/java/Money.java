69c988204d362847a6f49b77e077fda28a3a243c
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 16:05:17 +0100

    Page-37 #8, Make a static method for dollar

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -6,4 +6,8 @@ public class Money {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -13,4 +13,11 @@ public class MoneyTest {
         assertFalse(new Franc(5).equals(new Franc(6)));
         assertFalse(new Franc(5).equals(new Dollar(5)));
     }
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = Money.dollar(5); Money.dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
 }
