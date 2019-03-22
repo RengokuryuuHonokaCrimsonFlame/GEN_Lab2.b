bf0b40554c49c406c1f438f8731c064203a280ae
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 16:27:50 +0100

    Page-40 #9, Add currency in subclass

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,7 +1,10 @@
 class Franc extends Money {
 
+    private String currency;
+
     Franc(int amount) {
         this.amount= amount;
+        currency = "CHF";
     }
 
     Money times(int multiplier) {
@@ -9,6 +12,6 @@ class Franc extends Money {
     }
 
     String currency() {
-        return "CHF";
+        return currency;
     }
 }
