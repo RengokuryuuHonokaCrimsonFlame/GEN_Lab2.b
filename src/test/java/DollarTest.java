d9e5e26a7a6ec9fd98ec3a329c8b71019f01073b
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 15:14:25 +0100

    Page-26 #4, changes in testMultiplication

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DollarTest {
     @Test
     public void testMultiplication() {
-        Dollar five= new Dollar(5);
-        Dollar product= five.times(2);
-        assertEquals(10, product.amount);
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
