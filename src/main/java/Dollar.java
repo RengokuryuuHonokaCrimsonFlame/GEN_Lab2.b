741d069779de9e32bf1ad3296b6c2cb3abc54c9e
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 14:27:35 +0100

    Page-21 #1, amount = amunt * 2

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,12 +1,12 @@
 public class Dollar {
 
-    int amount = 5 * 2;
+    int amount;
 
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times( int multiplier) {
-        amount= 5 * 2;
+        amount = amount * 2;
     }
 }
