962f9d70f930d18f954e4325efda66afbdfa8fc0
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 15:55:49 +0100

    Page-35 #7, move equals to money. fixes #7

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +10,7 @@ public class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));  }
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
+    }
 }
