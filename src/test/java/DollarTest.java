f3327180d2a9744acf15cad1774f1ecae7a93edb
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-22 15:32:17 +0100

    Page-30 #5, add franc class and tests. fixes #5

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,7 +6,6 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
