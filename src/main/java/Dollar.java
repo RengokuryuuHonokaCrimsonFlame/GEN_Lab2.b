8900da9a4a373275bae6d9cbf9d39da32e03f2aa
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-23 13:52:05 +0100

    Page-47 #10, make dollar the same

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,6 +5,6 @@ public class Dollar extends Money{
     }
 
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier, currency);
+        return new Money(amount * multiplier, currency);
     }
 }
