10d295ef3add4e2781c7c309f462dace9a2a2048
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-23 13:50:51 +0100

    Page-47 #10, change equals method and add one test

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -5,6 +5,6 @@ class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return new Franc(amount * multiplier, currency);
+        return new Money(amount * multiplier, currency);
     }
 }
