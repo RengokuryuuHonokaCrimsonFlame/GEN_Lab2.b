fe80d17e314f7234fed33f89642742c6d47215e8
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-23 13:34:54 +0100

    Page-45 #10, I've return times to return new... And delete currency() from dollar (seems that I've forgot to drop it in the last chapter)

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,10 +5,6 @@ public class Dollar extends Money{
     }
 
     Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
+        return new Dollar(amount * multiplier, "USD");
     }
 }
--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -5,6 +5,6 @@ class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 }
