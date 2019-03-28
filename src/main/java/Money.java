3c3ca29157a0d81b0f1043030fff79f8b77f0621
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-28 09:17:04 +0100

    Page-65 #14, change the Plus method in Money to return an Expression

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -27,7 +27,7 @@ class Money implements Expression {
         return amount == money.amount && currency().equals(money.currency());
     }
 
-    Expression plus(Money addend) {
+    Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
