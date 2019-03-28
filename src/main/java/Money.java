e1c887e3c9c22f83f3325d370f28a5146debffcd
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-28 08:35:09 +0100

    Page-63 #13, I've add a Pair fonctionnality. fixes #13

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -13,12 +13,14 @@ class Money implements Expression {
         return new Money(amount * multiplier, currency);
     }
 
-    Money reduce(Expression source, String to) {
-        return source.reduce(to);
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     public Money reduce(String to){
-        return this;
+        int rate = (currency.equals("CHF") & to.equals("USD")) ? 2 : 1;
+        return new Money(amount / rate, to);
     }
 
     String currency() {
