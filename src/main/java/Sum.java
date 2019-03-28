92deb504f14c6d4967a29b41d95f7e80c3b13066
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-28 09:12:27 +0100

    Page-65 #14, change both sum attributes into Expression type

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,6 +1,6 @@
 public class Sum implements Expression {
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
     Sum(Money augend, Money addend) {
         this.augend = augend;
@@ -9,5 +9,6 @@ public class Sum implements Expression {
 
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
-        return new Money(amount, to);  }
+        return new Money(amount, to);
+    }
 }
